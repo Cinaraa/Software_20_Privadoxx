c7c079a806545ec2c7667645b288616f128d9e35
unknown <beatriz.goycoolea@uc.cl>
2022-09-28 16:57:33 -0300

    avanzado en la creacion de post

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   get 'homes/index'
-  get 'homes/show'
-  get 'homes/edit'
-  get 'homes/update'
-  root to: 'homes#index'
+  resources :articles
+  root 'homes#index'
+  # get 'homes/show'
+  # get 'homes/edit'
+  # get 'homes/update'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
